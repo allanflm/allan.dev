3f9ea5bae0ec3f49cab51096dd5a982dc202020e
Allan <allanfelipedk@gmail.com>
2026-02-05 17:00:56 -0300

    feat: refine hero layout, spacing and smooth scroll

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const textElement = document.getElementById("typing-text");
-const name = "Allan"; 
+const name = "Allan Felipe :)"; 
 let index = 0;
 
 function typeAnimation() {
@@ -10,3 +10,14 @@ function typeAnimation() {
     }
 }
 window.onload = typeAnimation;
+
+
+const scrollArrow = document.getElementById("scroll-to-about");
+const aboutSection = document.getElementById("about");
+
+scrollArrow.addEventListener("click", () => {
+    aboutSection.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+});
